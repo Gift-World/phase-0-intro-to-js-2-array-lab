abcab93a10f56e06ac9294652f610b788e28e6b8
charles Gift <charlesgiftangila@gmail.com>
2024-06-20 23:32:08 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(Gift){
+    cats.push(Gift);
+}
+function destructivelyPrependCat(Charles){
+    cats.unshift(Charles);
+}
+console.log(cats);
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function appendCat(Peter) {
+    return [...cats, Peter];
+  }
+  function destructivelyRemoveFirstCat(){
+    cats.shift();
+  }
+  function removeFirstCat() {
+    return cats.slice(1);
+  }
+  function prependCat(Ritchie) {
+    return [Ritchie, ...cats];
+  }
+  function removeLastCat() {
+    return cats.slice(0, -1);
+  }
+  
